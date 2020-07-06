32d373044da968c63b53e5527c54996bf53f954d
Ian Muyumba <ianmandela@gmail.com>
2020-07-06 19:08:43 +0300

    Initial commit

---
+++ introduction/intro.py
@@ -0,0 +1,18 @@
+print ("Ian")
+print(10+10)
+
+# Install a virtual environment
+
+
+"""
+
+Variable Naming Rules
+	* Should not contain spaces
+			Recommendation: 1. snake_type eg total_marks, student_name
+							2. camelCase eg totalMarks, studentName
+
+	* Should not start with a number eg 5students --> wrong
+	* Should not contain any special characters eg !"$^&%*"
+	* Should not be a keyword
+	
+"""
