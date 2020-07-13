42d39616fcf99013efc4c0b0752ab9c3fb330c27
Ian Muyumba <ianmandela@gmail.com>
2020-07-13 19:10:48 +0300

    flow control, test exercise

---
+++ week_2/flow_control/exercises.py
@@ -0,0 +1,39 @@
+"""
+    - Create an application that asks you to create an account for Jamii Bora APP
+        Provide your first name, last name, DOB, email and password
+
+        -------------- WELCOME TO JAMII BORA --------------
+        CREATE YOUR ACCOUNT
+        Enter your First Name
+        Enter your Last Name
+        Enter your DOB
+        Enter your Email
+        Enter your Password
+
+    - After creating the account, it should ask you to login
+
+        -------------- PLEASE LOGIN --------------
+        EMAIL ADDRESS:
+        PASSWORD:
+
+    - Validate the credentials
+    - If all are valid, print "Welcome J.D Peter"
+    - Print "Your account balance is 20,000"
+
+"""
+
+
+"""
+    - Create a primary school grading system
+        Ask for the Student Name, Class, and Score for 5 subjects (Maths, English, Kiswahili, Science, SST)
+
+    OUTPUT
+
+        -------------- RESULTS CARD --------------
+        Student Name: Ian Muyumba
+        Class: 5
+        Total Marks: 400
+        Average Score: 80
+        Grade: A
+
+"""
--- week_2/flow_control/intro.py
+++ week_2/flow_control/intro.py
@@ -51,16 +51,49 @@
 
 # Nested If statements - check if the number is positive or negative or zero
 # Indentation is the key in nested statements
+#
+# num = float(input("Enter a number: "))
+#
+# if num >= 0: # If the expression is false, the nested statement wont be executed
+#     if num == 0:
+#         print("Zero")
+#     else:
+#         print("Positive number")
+# else:
+#     print("Negative number")
+#
 
-num = float(input("Enter a number: "))
+"""
+    ITERATION CONTROL STRUCTURES
+        * for...loop
+            Syntax:
 
-if num >= 0: # If the expression is false, the nested statement wont be executed
-    if num == 0:
-        print("Zero")
-    else:
-        print("Positive number")
-else:
-    print("Negative number")
+                for val in sequence:
+                    Body of for
+
+                    N/B
+                - Loop continues until we reach the last item in the sequence
+                - The body of for loop is separated from the rest of the code using indentation
+        * while ...loop
+"""
+
+#for...loop
+
+# name = "Ian"
+# for char in name:
+#     print(char)
+
+# Check for the highest number using nested for loop and if statement
+ages = [10,20,30,60,50,40]
+highestAge = 0
+
+for age in ages:
+    if age > highestAge:
+        highestAge = age
+
+print(highestAge)
+
+
 
 
 
